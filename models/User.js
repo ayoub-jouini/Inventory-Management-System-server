316e287d899146d6636ccfdc81bd86cc300eaa54
ayoub-jouini <ayoubjouini135@gmail.com>
2022-10-16 12:54:28 +0100

    user schema

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phoneNB: { type: String, required: true },
+  role: { type: String, required: true },
+  password: { type: String, required: true },
+  avatar: { type: String, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
